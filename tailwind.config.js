c8fe39151a7098f08034aa2e1b048f7fa3093ec9
Marius_GBK <mariusbiaou75@gmail.comgit config --global user.email mariusbiaou75@linux.org>
2025-08-31 00:23:42 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+ /** @type {import('tailwindcss').Config} */
+export default {
+   content: ["./src/**/*.{html, css, js}"],
+   theme: {
+     extend: {
+      fontFamily: {
+        jakarta: ['"Plus Jakarta Sans"', 'sans-serif'],
+     },
+   },
+  },
+   plugins: [],
+ }
